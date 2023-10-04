f9bdf344a865a7f587ef58f52cab81898044224a
akarshkathal <akarshkathal@gmail.com>
2023-10-04 13:43:42 -0700

    Revert "Added name Mike Koenig to IndexModel Comment in Index.cshtml.cs"
    This reverts commit b5bd68102a8b21897352dec1b5d205fe7f193d4d.

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Akarsh Kathal
+    /// Ritu Priya
     /// </summary>
     public class IndexModel : PageModel
     {
