87e12fbb3e453bb3d8972a6babc4e1ef896c8ebb
ritu-priya03 <ritupriya215@gmail.com>
2023-10-04 13:18:09 -0700

    Update Index.cshtml.cs
    Added name Ritu Priya to IndexModel Comment in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Mike Koenig
+    /// Ritu Priya
     /// </summary>
     public class IndexModel : PageModel
     {
