ee0deaba1f3f1066cdade6d5655c354542eec006
Vandana-source <145959980+Vandana-source@users.noreply.github.com>
2023-10-04 13:51:42 -0700

    Added name Vandana Gandepalli to IndexModel Comment in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Akarsh Kathal
+    /// Vandana Gandepalli
     /// </summary>
     public class IndexModel : PageModel
     {
