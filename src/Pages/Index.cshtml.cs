98623f9f66231d30b729ef84845377ecf09d3c38
Dhruv Patel <19bt04031@gsfcuniversity.ac.in>
2023-10-04 14:00:59 -0700

    Added name Dhruv Patel to IndexModel Comment in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Vandana Gandepalli
+    /// Dhruv Patel
     /// </summary>
     public class IndexModel : PageModel
     {
