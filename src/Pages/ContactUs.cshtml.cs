80bb8086f8adf1a95ccbdc1596ec5201e1e67b2a
akarshkathal <akarshkathal@gmail.com>
2023-10-15 17:38:02 -0700

    Creating ContactUs.cshtml for a newly adding ContactUs button in pages folder

---
+++ src/Pages/ContactUs.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class ContactUsModel : PageModel
+    {
+        private readonly ILogger<ContactUsModel> _logger;
+
+        public ContactUsModel(ILogger<ContactUsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
