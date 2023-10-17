404bd44a760232f8f501277e05d65c8905e03a2a
myasha <myasha@gmail.com>
2023-10-16 23:51:42 -0700

    added more maps to products.json

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -9,16 +10,18 @@ namespace ContosoCrafts.WebSite.Models
         public string LocationType { get; set; }
 
         [JsonPropertyName("img")]
-        public string Image { get; set; }
+        public string Image { get; set;}
         public string Url { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Neighborhood { get; set; }
         public string MapURL { get; set; }
         public string FunFacts { get; set; }
-        public string Address {get; set;}
+        public string Address {get; set; }
         public int[] Ratings {get; set; }
         public string Accessibility { get; set; }
+        public string OpeningHours { get; set; }
+        public DateTime LastUpdated { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
