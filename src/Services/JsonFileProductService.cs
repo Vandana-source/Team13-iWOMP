c3218783bcb3b3fb8da578725e0cafcd8cad5e92
Vandana-source <145959980+Vandana-source@users.noreply.github.com>
2023-10-22 22:06:30 -0700

    Added UpdateData and SaveData methods in JsonFileProductService.cs

--- src/Services/JsonFileProductService.cs
+++ src/Services/JsonFileProductService.cs
@@ -6,10 +6,11 @@ using System.Text.Json;
 using ContosoCrafts.WebSite.Models;
 
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace ContosoCrafts.WebSite.Services
 {
-   public class JsonFileProductService
+    public class JsonFileProductService
     {
         public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
         {
@@ -25,7 +26,7 @@ namespace ContosoCrafts.WebSite.Services
 
         public IEnumerable<ProductModel> GetProducts()
         {
-            using(var jsonFileReader = File.OpenText(JsonFileName))
+            using (var jsonFileReader = File.OpenText(JsonFileName))
             {
                 return JsonSerializer.Deserialize<ProductModel[]>(jsonFileReader.ReadToEnd(),
                     new JsonSerializerOptions
@@ -39,7 +40,7 @@ namespace ContosoCrafts.WebSite.Services
         {
             var products = GetProducts();
 
-            if(products.First(x => x.Id == productId).Ratings == null)
+            if (products.First(x => x.Id == productId).Ratings == null)
             {
                 products.First(x => x.Id == productId).Ratings = new int[] { rating };
             }
@@ -50,17 +51,51 @@ namespace ContosoCrafts.WebSite.Services
                 products.First(x => x.Id == productId).Ratings = ratings.ToArray();
             }
 
-            using(var outputStream = File.OpenWrite(JsonFileName))
+            using (var outputStream = File.OpenWrite(JsonFileName))
             {
                 JsonSerializer.Serialize<IEnumerable<ProductModel>>(
                     new Utf8JsonWriter(outputStream, new JsonWriterOptions
                     {
                         SkipValidation = true,
                         Indented = true
-                    }), 
+                    }),
                     products
                 );
             }
         }
+
+        public ProductModel UpdateData(ProductModel data)
+        {
+            var products = GetProducts();
+            var productData = products.FirstOrDefault(x => x.Id.Equals(data.Id));
+
+            // Update the data to the new passed in values
+            productData.Title = data.Title;
+            productData.LocationType = data.LocationType;
+            productData.Neighborhood = data.Neighborhood;
+            productData.Description = data.Description;
+            productData.MapURL = data.MapURL;
+
+            SaveData(products);
+
+            return productData;
+
+        }
+
+        private void SaveData(IEnumerable<ProductModel> products)
+        {
+            using (var outputStream = File.Create(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<ProductModel>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    products
+                );
+            }
+        }
+
     }
 }
