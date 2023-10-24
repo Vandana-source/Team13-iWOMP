caa0b9c1e45eed9b1ffb3fe8f05ca6b70fcab31f
ritu-priya03 <ritupriya215@gmail.com>
2023-10-24 12:23:11 -0700

    Added test cases for OnGet method in Update.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace UnitTests.Pages.Product.Update
+{
+    internal class ProductModel : ContosoCrafts.WebSite.Models.ProductModel
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ UnitTests/Pages/Product/Update.cshtml.Tests.cs
@@ -0,0 +1,129 @@
+﻿using System.Linq;
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Services;
+
+
+namespace UnitTests.Pages.Product.Update
+{
+    public class UpdateTests
+    {
+        #region TestSetup
+        public static IUrlHelperFactory urlHelperFactory;
+        public static DefaultHttpContext httpContextDefault;
+        public static IWebHostEnvironment webHostEnvironment;
+        public static ModelStateDictionary modelState;
+        public static ActionContext actionContext;
+        public static EmptyModelMetadataProvider modelMetadataProvider;
+        public static ViewDataDictionary viewData;
+        public static TempDataDictionary tempData;
+        public static PageContext pageContext;
+
+        public static UpdateModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            httpContextDefault = new DefaultHttpContext()
+            {
+                //RequestServices = serviceProviderMock.Object,
+            };
+
+            modelState = new ModelStateDictionary();
+
+            actionContext = new ActionContext(httpContextDefault, httpContextDefault.GetRouteData(), new PageActionDescriptor(), modelState);
+
+            modelMetadataProvider = new EmptyModelMetadataProvider();
+            viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            tempData = new TempDataDictionary(httpContextDefault, Mock.Of<ITempDataProvider>());
+
+            pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData,
+            };
+
+            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
+            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
+            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net7.0/wwwroot");
+            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
+
+            var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
+            JsonFileProductService productService;
+
+            productService = new JsonFileProductService(mockWebHostEnvironment.Object);
+
+            pageModel = new UpdateModel(productService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("SeattleUQuadTable1");
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("Seattle University Green", pageModel.Product.Title);
+        }
+
+        [Test]
+        public void OnGet_InValid_Should_Not_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("Test");
+
+            // Assert
+            Assert.IsNull(pageModel.Product);
+        }
+
+        [Test]
+        public void OnGet_Null_Id_Should_Return_False()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("");
+
+            // Assert
+            Assert.IsNull(pageModel.Product);
+        }
+
+        [Test]
+        public void OnGet_Valid_Id_Null_Title_Should_Return_False()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet("SeattleUQuadTable1");
+
+            // Assert
+            Assert.AreNotEqual("", pageModel.Product.Title);
+        }
+        #endregion OnGet
+
+ 
+    }
+}
