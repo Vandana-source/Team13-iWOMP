c4a8c288fcb8c9e33c9892ca38e1d8c9bc978425
Vandana-source <145959980+Vandana-source@users.noreply.github.com>
2023-10-23 23:16:19 -0700

    Added test cases for AddRating method in JsonFileProductServiceTests.cs

--- UnitTests/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -1,21 +1,22 @@
-﻿using System.Linq;
-
-using Microsoft.AspNetCore.Mvc;
-
+﻿using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
-using Moq; 
-
+using ContosoCrafts.WebSite.Pages.Product;
 using ContosoCrafts.WebSite.Models;
-using ContosoCrafts.WebSite.Services;
-using System.Collections.Generic;
+using System.Linq;
 
 namespace UnitTests.Pages.Product.AddRating
 {
+
+    /// <summary>
+    /// Class containing unit test cases to JsonFileProductService file
+    /// </summary>
     public class JsonFileProductServiceTests
     {
-
         #region TestSetup
 
+        /// <summary>
+        /// Test initialize
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -23,44 +24,121 @@ namespace UnitTests.Pages.Product.AddRating
 
         #endregion TestSetup
 
-        #region AddRating
 
+        #region AddRating
         /// <summary>
-        /// For a valid product and ID:
-        /// - Tests if ratings array is null, one is created.
-        /// - Sees if rating is added correctly  
+        ///Testing to check if a invalid product null will return false
         /// </summary>
         [Test]
-        public void AddRating_Valid_Product_If_Ratings_Null_Should_Create_Ratings_Array_And_Add_Rating_Successfully()
+        public void AddRating_Invalid_Product_Null_Should_Return_False()
         {
             // Arrange
 
-
             // Act
+            var result = TestHelper.ProductService.AddRating(null, 1);
 
-
-            // Assert 
-
-
+            // Assert
+            Assert.AreEqual(false, result);
         }
 
-
-        // <summary>
-        // If ratings is not null, checks if rating was added to the array successfully 
-        // </summary>     
+        /// <summary>
+        ///Testing AddRating for invalid product empty will return false
+        /// </summary>
         [Test]
-        public void AddRating_Valid_Product_If_Ratings_Not_Null_Should_Add_Rating_To_Array_Successfully()
+        public void AddRating_Invalid_Product_Empty_Should_Return_False()
         {
             // Arrange
 
             // Act
+            var result = TestHelper.ProductService.AddRating(null, 1);
 
-            // Assert 
-
+            // Assert
+            Assert.AreEqual(false, result);
         }
 
-        #endregion 
+        /// <summary>
+        ///Testing AddRating for invalid null data should return false
+        /// </summary>
+        [Test]
+        public void AddRating_Invalid_Data_Null_Should_Return_False()
+        {
+            // Arrange
+
+            // Act
+            var result = TestHelper.ProductService.AddRating("Not exists", 3);
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+        /// <summary>
+        /// Testing AddRating for invalid negative rating should return false
+        /// </summary>        
+        [Test]
+        public void AddRating_Invalid_Negative_Rating_Should_Return_False()
+        {
+            // Arrange
+            var productId = TestHelper.ProductService.GetProducts().First().Id;
+
+            // Act
+            var result = TestHelper.ProductService.AddRating(productId, -3);
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+        /// <summary>
+        /// Testing AddRating for Invalid rating greater then 5 should return false
+        /// </summary>
+        [Test]
+        public void AddRating_Invalid_Rating_Greater_Than_Five_Should_Return_False()
+        {
+            // Arrange
+            var productId = TestHelper.ProductService.GetProducts().First().Id;
+
+            // Act
+            var result = TestHelper.ProductService.AddRating(productId, 8);
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+        /// <summary>
+        /// Testing AddRating for valid product and rating array null should create new array and return true
+        /// </summary>
+        [Test]
+        public void AddRating_Valid_Rating_Array_Is_Null_Should_Create_New_Array_Return_True()
+        {
+            // Arrange
+
+            // Act
+            var result = TestHelper.ProductService.AddRating("starbucks-reserve", 5);
+
+            // Assert
+            Assert.AreEqual(true, result);
+        }
+
+        /// <summary>
+        /// Testing AddRating for valid product and rating should return true
+        /// </summary>
+        [Test]
+        public void AddRating_Valid_Product_And_Rating_Should_Return_True()
+        {
+            // Arrange
+            var data = TestHelper.ProductService.GetProducts().FirstOrDefault(x => x.Id.Equals("u-village"));
+            var count = data.Ratings.Length;
+
+            // Act
+            var result = TestHelper.ProductService.AddRating(data.Id, 5);
+            var newData = TestHelper.ProductService.GetProducts().FirstOrDefault(x => x.Id.Equals("u-village"));
+
+            // Assert
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(count + 1, newData.Ratings.Length);
+            Assert.AreEqual(5, newData.Ratings.Last());
+        }
+
+        #endregion AddRating
 
     }
-
 }
