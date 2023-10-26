669e3dfa599a63d3c573f12bc68ed6852d4d30c0
myasha <myasha@gmail.com>
2023-10-26 11:40:37 -0700

    Added comments to Error.cshml.cs

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -6,22 +6,37 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    ///  Represents the error page model for the ContosoCrafts website.
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
+        // Gets or dets the id of the current request
         public string RequestId { get; set; }
 
+        // Determines whether to show the RequestId or not
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+        // Logger instance to log error details
         private readonly ILogger<ErrorModel> _logger;
 
+
+        /// <summary>
+        /// Initializes a new instance of the ErrorModel class
+        /// </summary>
         public ErrorModel(ILogger<ErrorModel> logger)
         {
+            // Instsance for error logging details 
             _logger = logger;
         }
 
+        /// <summary>
+        /// Method executed on GET request to the error page.
+        /// </summary>
         public void OnGet()
         {
+            // Sets the RequestId property.
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
         }
     }
