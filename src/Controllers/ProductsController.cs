92aebaae5ec0832e136d383fde3d7a7bc2b005c6
Vandana-source <145959980+Vandana-source@users.noreply.github.com>
2023-10-26 17:01:48 -0700

    Added comments to ProductsController.cs

--- src/Controllers/ProductsController.cs
+++ src/Controllers/ProductsController.cs
@@ -9,32 +9,59 @@ namespace ContosoCrafts.WebSite.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    /// <summary>
+    /// Get the root path for ProductsController file
+    /// </summary>
     public class ProductsController : ControllerBase
     {
+
+        /// <summary>
+        /// Constructor for ProductsController
+        /// </summary>
+        /// <param name="productService"></param>
         public ProductsController(JsonFileProductService productService)
         {
             ProductService = productService;
         }
 
+        // Getter for ProductService
         public JsonFileProductService ProductService { get; }
 
         [HttpGet]
+        /// <summary>
+        /// GetProducts method is used to get the list of all the products, it parses the JSON file and converts into products model list
+        /// </summary>
+        /// <returns>List of Product Model</returns>
         public IEnumerable<ProductModel> Get()
         {
             return ProductService.GetProducts();
         }
 
         [HttpPatch]
+        /// <summary>
+        /// Add rating to specific product selected
+        /// </summary>
         public ActionResult Patch([FromBody] RatingRequest request)
         {
+            // Adding rating
             ProductService.AddRating(request.ProductId, request.Rating);
             
             return Ok();
         }
 
+        /// <summary>
+        /// Get Rating request by product Id
+        /// </summary>
         public class RatingRequest
         {
+            /// <summary>
+            /// Getter and Setter for ProductId
+            /// </summary>
             public string ProductId { get; set; }
+
+            /// <summary>
+            /// Getter and Setter for Rating
+            /// </summary>
             public int Rating { get; set; }
         }
     }
