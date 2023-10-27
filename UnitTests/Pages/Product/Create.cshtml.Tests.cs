fc6029afb1b904bd2e3409c8468e7deb66f1a186
myasha <myasha@gmail.com>
2023-10-26 18:18:41 -0700

    added Create.cshtml.Tests.cs to unit tests/products.

---
+++ UnitTests/Pages/Product/Create.cshtml.Tests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Models;
+
+
+namespace UnitTests.Pages.Product.Update
+{
+    public class CreateTests
+    {
+    }
+}
