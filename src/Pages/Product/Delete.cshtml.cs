62f080b4a8541c7c29026e5537339d39ef03f8ff
myasha <myasha@gmail.com>
2023-10-27 20:34:30 -0700

    created delete html page in pages/products

---
+++ src/Pages/Product/Delete.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages.Product;
+
+public class Delete : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
