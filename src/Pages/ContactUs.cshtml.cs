aba659237f77b6109d679bff4902f3cb5b23afe5
Dhruv Patel <19bt04031@gsfcuniversity.ac.in>
2023-10-29 10:03:25 -0700

    Added comments in the file ContactUs.cshtml.cs

--- src/Pages/ContactUs.cshtml.cs
+++ src/Pages/ContactUs.cshtml.cs
@@ -3,17 +3,26 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+
+    /// <summary>
+    /// Represents the code-behind for the ContactUs Razor Page.
+    /// </summary>
     public class ContactUsModel : PageModel
     {
+        // Declare a private field to hold a logger instance
         private readonly ILogger<ContactUsModel> _logger;
 
+        // Constructor for ContactUsModel, takes a logger as a dependency
         public ContactUsModel(ILogger<ContactUsModel> logger)
         {
+            // Initialize the logger field with the injected logger
             _logger = logger;
         }
 
         public void OnGet()
         {
+            // This is the PageModel's method called when an HTTP GET request is made to this page.
+            // It is typically used to prepare data for display, but in this case, it's empty, so no specific action is taken.
         }
     }
 }
