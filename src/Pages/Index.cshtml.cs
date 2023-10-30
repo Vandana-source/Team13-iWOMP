ff1c888dc2d1a1363263a86d1ae878a8420dc1e5
ritu-priya03 <ritupriya215@gmail.com>
2023-10-30 16:08:56 -0700

    Added Comments to Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -8,17 +8,25 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Represents the code-behind for the Index Razor Page.
+    /// </summary>
     public class IndexModel : PageModel
     {
+        // Declare a private field to hold a logger instance
         private readonly ILogger<IndexModel> _logger;
 
+        // Constructor for IndexModel, takes a logger as a dependency
         public IndexModel(ILogger<IndexModel> logger)
         {
+            // Initiate the logger field with the injected logger
             _logger = logger;
         }
 
         public void OnGet()
         {
+            // This is the IndexeModel's method called when an HTTP GET request is made to this page.
+            // It is typically used to prepare data for display, but in this case, it's empty, so no specific action is taken.
         }
     }
 }
