63a95ccc18ef62200023d48be88750b89f6ac633
Dhruv Patel <19bt04031@gsfcuniversity.ac.in>
2023-10-31 01:41:02 -0700

    Added directives, BindProperty and Condition for upload image logic in the file Create.cshtml.cs

--- src/Pages/Product/Create.cshtml.cs
+++ src/Pages/Product/Create.cshtml.cs
@@ -2,55 +2,77 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ContosoCrafts.WebSite.Services;
 using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System;
+
 namespace ContosoCrafts.WebSite.Pages.Product
 {
-
-    /// <summary>
-    /// Create Page
-    /// </summary>
     public class CreateModel : PageModel
     {
-
-        // Data middle tier
         public JsonFileProductService ProductService { get; }
+        private readonly IWebHostEnvironment _hostingEnvironment;
 
-        /// <summary>
-        /// Defualt Constructor
-        /// </summary>
-        /// <param name="productService"></param>
-        /// <param name="logger"></param>
-        public CreateModel(JsonFileProductService productService)
+        [BindProperty] public ProductModel Product { get; set; }
+        [BindProperty] public IFormFile UploadedImage { get; set; }
+
+        public CreateModel(JsonFileProductService productService,
+            IWebHostEnvironment hostingEnvironment)
         {
             ProductService = productService;
+            _hostingEnvironment = hostingEnvironment;
         }
 
-        // The data to show
-        // ProductModel
-        [BindProperty]
-        public ProductModel Product { get; set; }
+        public IFormFile UploadedFile { get; set; }
 
-        /// <summary>
-        /// Handles the GET request for the Create page.
-        /// Currently, no specific action is required before rendering the view.
-        /// This method is kept for clarity and potential future use.
-        /// </summary>
         public void OnGet()
         {
         }
 
-        /// <summary>
-        /// Post the model back to the page
-        /// The model is in the class variable Product
-        /// Call the data layer to Update that data
-        /// Then return to the Index page
-        /// </summary>
-        /// <returns></returns>
         public IActionResult OnPost()
         {
-            // Update data into database
-            ProductService.CreateData(Product);
+            if (UploadedFile != null && UploadedFile.Length > 0)
+            {
+                // Define the directory based on LocationType
+                string subDirectory = Product.LocationType switch
+                {
+                    "Table" => "Tables",
+                    "Bench" => "Benches",
+                    "Restroom" => "Restrooms",
+                    _ => "Others"
+                };
 
-            return RedirectToPage("./Index");
+                // Create a unique filename for the uploaded file
+                string uniqueFileName = Guid.NewGuid().ToString() + "_" +
+                                        UploadedFile.FileName;
+
+                // Get the path for saving the file inside the SiteImages folder, then the desired subdirectory within wwwroot
+                string savePath = Path.Combine(_hostingEnvironment.WebRootPath,
+                    "SiteImages", subDirectory, uniqueFileName);
+
+                // Ensure the directory exists, if not, create it
+                Directory.CreateDirectory(Path.GetDirectoryName(savePath));
+
+                // Save the uploaded file to the server
+                using var fileStream =
+                    new FileStream(savePath, FileMode.Create);
+                UploadedFile
+                    .CopyTo(
+                        fileStream); // Note: Using the synchronous method here
+
+                // Update the Product.Image property with the relative path to the saved file
+                Product.Image = Path.Combine("/SiteImages", subDirectory, uniqueFileName);
+
+                // Save the product to the database (or in this case, the JSON file)
+                ProductService.CreateData(Product);
+
+                // Redirect to a success or product listing page after saving
+                return RedirectToPage("Index"); // Assuming you have an Index page for listing products
+            }
+
+            // Stay on the same page if no file was uploaded or there was an issue
+            return Page();
         }
     }
 }
