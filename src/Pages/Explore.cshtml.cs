dc098dfa0917fa673ef2cac148f8ae3569c27e59
akarshkathal <akarshkathal@gmail.com>
2023-10-30 23:48:57 -0700

    Changing onGet method so that it retrieves the data according to the filter in Explore.cshtml.cs

--- src/Pages/Explore.cshtml.cs
+++ src/Pages/Explore.cshtml.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -8,19 +10,15 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
-    /// <summary>
-    /// Represents the code-behind for the Explore Razor Page.
-    /// </summary>
     public class ExploreModel : PageModel
     {
-        // Declare a private field to hold a logger instance
         private readonly ILogger<ExploreModel> _logger;
 
-        // Constructor for ExploreModel, takes a logger as a dependency
+        public string LocationType { get; set; }
+
         public ExploreModel(ILogger<ExploreModel> logger,
             JsonFileProductService productService)
         {
-            // Initiate the logger field with the injected logger
             _logger = logger;
             ProductService = productService;
         }
@@ -28,10 +26,24 @@ namespace ContosoCrafts.WebSite.Pages
         public JsonFileProductService ProductService { get; }
         public IEnumerable<ProductModel> Products { get; private set; }
 
-        public void OnGet()
+        public void OnGet(string LocationType)
         {
-            // This method fetches all the data from the JsonFileProductService.cs
-            Products = ProductService.GetProducts();
+            var allProducts = ProductService.GetProducts();
+
+            // Check if location is not null or empty
+            if (!LocationType.Equals("All"))
+            {
+                // Filter products based on location
+                // Leaving it location for now: need to add logic to do others
+
+                Products = allProducts.Where(p => p.LocationType == LocationType);
+            }
+            else
+            {
+                // If location is null or empty, return all products
+                Products = allProducts.Take(36);
+            }
         }
+
     }
 }
