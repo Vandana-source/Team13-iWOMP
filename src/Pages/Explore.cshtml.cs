4c19f0367d0e4346c290bb363ae4ddd0d15c4553
akarshkathal <akarshkathal@gmail.com>
2023-10-31 10:39:23 -0700

    Adding Comments for better understanding in Explore.cshtml.cs

--- src/Pages/Explore.cshtml.cs
+++ src/Pages/Explore.cshtml.cs
@@ -10,12 +10,21 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Explore Page will return all the data to show according to filter
+    /// </summary>
     public class ExploreModel : PageModel
     {
+        //Logger 
         private readonly ILogger<ExploreModel> _logger;
 
+        //Location Type of product
         public string LocationType { get; set; }
 
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
         public ExploreModel(ILogger<ExploreModel> logger,
             JsonFileProductService productService)
         {
@@ -23,24 +32,28 @@ namespace ContosoCrafts.WebSite.Pages
             ProductService = productService;
         }
 
+        // Data Service
         public JsonFileProductService ProductService { get; }
+
+        //Collection of products
         public IEnumerable<ProductModel> Products { get; private set; }
 
+        /// <summary>
+        /// REST OnGet, return all data based on the locationtype 
+        /// </summary>
         public void OnGet(string LocationType)
         {
             var allProducts = ProductService.GetProducts();
 
-            // Check if location is not null or empty
+            // Check if location type is All or not
             if (!LocationType.Equals("All"))
             {
                 // Filter products based on location
-                // Leaving it location for now: need to add logic to do others
-
                 Products = allProducts.Where(p => p.LocationType == LocationType);
             }
             else
             {
-                // If location is null or empty, return all products
+                // Returns all products
                 Products = allProducts.Take(36);
             }
         }
