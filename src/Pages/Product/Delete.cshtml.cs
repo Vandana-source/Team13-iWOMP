b0c3550712e4af3214d9785cd1c8498c12a5e40d
akarshkathal <akarshkathal@gmail.com>
2023-10-31 10:35:48 -0700

    Adding comments for better readability in Delete.cshtml.cs

--- src/Pages/Product/Delete.cshtml.cs
+++ src/Pages/Product/Delete.cshtml.cs
@@ -9,32 +9,49 @@ using Microsoft.AspNetCore.Mvc;
 namespace ContosoCrafts.WebSite.Pages.Product
 
 {
+    /// <summary>
+    /// Delete Page will delete the data from json file
+    /// </summary>
     public class DeleteModel : PageModel
     {
+        //Data Service
         public JsonFileProductService ProductService { get; }
-        
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
         public DeleteModel(JsonFileProductService productService)
         {
             ProductService = productService;
         }
 
+        // Collection of the Data
         [BindProperty]
         public ProductModel Product { get; set; }
 
+        /// <summary>
+        /// REST OnGet, return all data
+        /// </summary>
         public void OnGet(string id)
         {
+            // This method fetches all the data from the JsonFileProductService.cs
             Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
         }
 
+        /// <summary>
+        /// OnPost Method, will get the product which should be deleted from DB
+        /// </summary>
         public IActionResult OnPost()
         {
+            // This method will check if the Model is valid
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-
+            // This method will call the JsonFileProductService.cs Delete method which will delete the product
             ProductService.DeleteData(Product);
-
+            // This will redirect to the Index page once the data is deleted.
             return RedirectToPage("./Index");
         }
     }
