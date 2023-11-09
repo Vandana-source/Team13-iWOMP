800478022289b04fa99dec8666da3f47e1d53227
myasha <myasha@gmail.com>
2023-11-09 10:40:22 -0800

    Added properties to CustomerModel.cs

--- src/Models/CustomerModel.cs
+++ src/Models/CustomerModel.cs
@@ -1,17 +1,45 @@
-namespace ContosoCrafts.WebSite.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
-/// <summary>
-/// A class to store contact details 
-/// </summary>
-public class CustomerModel
+namespace ContosoCrafts.WebSite.Models
+
 {
-    
-    // Customer first name
-    
-    // Customer last name
-    
-    // Customer contact email
+    /// <summary>
+    /// A class to store contact details and nominated place details from customer
+    /// </summary>
+    public class CustomerModel
+    {
+        // Customer first name
+        public string CusFirstName { get; set; }
 
-    // What other details to add? 
+        // Customer last name
+        public string CusLastName { get; set;}
+
+        // Customer contact email
+        public string CusEmail { get; set;}
+        
+        // Nominated title of the new place 
+        public string NominatedTitle { get; set; }
+        
+        // Nominated image url
+        public string NominatedImage { get; set; }
+        
+        // Nominated noise level
+        public int NominatedNoiseLevel { get; set;}
+        
+        // Nominated neighborhood
+        public string NominatedNeighborhoodP { get; set; }
+        
+        // Nominated map details 
+        public string NominatedMapDetails { get; set;}
+        
+        // Overrides the ToString method to serialize the object to JSON.
+        public override string ToString() => JsonSerializer.Serialize<CustomerModel>(this);
+    }
+    
+    
+    
     
 }
