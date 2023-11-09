f650b513ae22c52ae63fc92be494bec72edd4e48
myasha <myasha@gmail.com>
2023-11-09 10:30:45 -0800

    created customerModel.cs in Models

---
+++ src/Models/CustomerModel.cs
@@ -0,0 +1,17 @@
+namespace ContosoCrafts.WebSite.Models;
+
+/// <summary>
+/// A class to store contact details 
+/// </summary>
+public class CustomerModel
+{
+    
+    // Customer first name
+    
+    // Customer last name
+    
+    // Customer contact email
+
+    // What other details to add? 
+    
+}
