2d48567147c3c4db67070b66b990f000398b778c
myasha <myasha@gmail.com>
2023-11-09 17:01:38 -0800

    created JsonFileCustomerService.cs in services

---
+++ src/Services/JsonFileCustomerService.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+using ContosoCrafts.WebSite.Models;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+namespace ContosoCrafts.WebSite.Services
+{
+    public class JsonFileCustomerService
+    {
+        
+        /// <summary>
+        /// Setting the web hosting environment
+        /// </summary>
+        /// <param name="webHostEnvironment"></param>
+        public JsonFileCustomerService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+        
+        // Assigned IWebHostEnvironment to the public property
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        
+        
+        // Gets the combined path of JSON file
+        private string JsonFileName
+        {
+            get
+            {
+                return Path.Combine(WebHostEnvironment.WebRootPath, "data",
+                    "contactDetails.json");
+            }
+        }
+        
+        /// <summary>
+        /// Save all customer data to storage
+        /// </summary>
+        /// <param name="customers"></param>
+        private void SaveData(IEnumerable<CustomerModel> customers)
+        {
+            using (var outputStream = File.Create(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<CustomerModel>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    customers
+                );
+            }
+        }
+    }
+}
