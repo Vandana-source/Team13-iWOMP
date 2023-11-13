c1e7cc510145cd336ef2848e130f768b4978b4ec
myasha <myasha@gmail.com>
2023-11-13 12:15:15 -0800

    Updated ContactUs.cshtml.cs

--- src/Pages/ContactUs.cshtml.cs
+++ src/Pages/ContactUs.cshtml.cs
@@ -1,28 +1,97 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using TakeABreak.WebSite.Models;
+using TakeABreak.WebSite.Services;
 
 namespace TakeABreak.WebSite.Pages
 {
-
     /// <summary>
-    /// Represents the code-behind for the ContactUs Razor Page.
+    /// Page for adding customer nominations/contact details 
     /// </summary>
     public class ContactUsModel : PageModel
     {
-        // Declare a private field to hold a logger instance
+        // Service to handle customer data operations
+        public JsonFileCustomerService CustomerService { get; }
+
+        // Web hosting environment for file operations
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        // Logger for logging messages
         private readonly ILogger<ContactUsModel> _logger;
 
-        // Constructor for ContactUsModel, takes a logger as a dependency
-        public ContactUsModel(ILogger<ContactUsModel> logger)
+        // Model for customer nomination data binding
+        [BindProperty]
+        public CustomerModel CustomerNomination { get; set; }
+
+        /// <summary>
+        /// Constructor for ContactUsModel
+        /// </summary>
+        /// <param name="customerService">The customer service</param>
+        /// <param name="hostingEnvironment">The web hosting environment</param>
+        /// <param name="logger">The logger</param>
+        public ContactUsModel(JsonFileCustomerService customerService,
+            IWebHostEnvironment hostingEnvironment,
+            ILogger<ContactUsModel> logger)
         {
-            // Initialize the logger field with the injected logger
+            CustomerService = customerService;
+            _hostingEnvironment = hostingEnvironment;
             _logger = logger;
         }
 
+        /// <summary>
+        /// Handles HTTP GET request for the page
+        /// </summary>
         public void OnGet()
         {
-            // This is the PageModel's method called when an HTTP GET request is made to this page.
-            // It is typically used to prepare data for display, but in this case, it's empty, so no specific action is taken.
+            // PageModel's method for HTTP GET request handling.
+        }
+
+        /// <summary>
+        /// Handles HTTP POST request for the page
+        /// </summary>
+        /// <returns>ActionResult</returns>
+        public IActionResult OnPost()
+        {
+            // Validate the NominatedMapDetails URL
+            if (!IsAllowedMapURL(CustomerNomination.NominatedMapDetails))
+            {
+                ModelState.AddModelError("NominatedMapDetails", "Please provide a valid Google Maps URL");
+                return Page();
+            }
+
+            // Assuming the nominated image URL is provided directly in the CustomerNomination object
+            if (!string.IsNullOrEmpty(CustomerNomination.NominatedImage))
+            {
+                // Here you can add any logic needed for the nominated image URL,
+                // like validation or formatting.
+
+                // Save the customer data
+                CustomerService.AddCustomer(CustomerNomination);
+
+                // Redirect to a success or listing page after saving
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("NominatedImage", "Please provide a URL for the nominated image.");
+                return Page();
+            }
+        }
+
+        /// <summary>
+        /// Validates a Google Maps URL
+        /// </summary>
+        /// <param name="mapURL">The URL to validate</param>
+        /// <returns>True if the URL is valid, otherwise False</returns>
+        private bool IsAllowedMapURL(string mapURL)
+        {
+            string googleMapsPattern = @"^https://www\.google\.com/maps/embed\?pb=!.+$";
+            Regex regex = new Regex(googleMapsPattern);
+            return regex.IsMatch(mapURL);
         }
     }
 }
