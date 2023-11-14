e1b59dfa48ad28aada138ed5bf187e8fb5cca486
Vandana-source <145959980+Vandana-source@users.noreply.github.com>
2023-11-14 10:50:47 -0800

    Created and added unit tests for CustomerModel.cs in CustomerModelTests.cs

---
+++ UnitTests/Models/CustomerModelTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using TakeABreak.WebSite.Models;
+
+namespace UnitTests.Models.ToString
+{
+
+    /// <summary>
+    /// Class containing unit test cases to ProductModel
+    /// </summary>
+    public class CustomerModelTests
+    {
+        /// <summary>
+        /// Testing Json to String serializer
+        /// </summary>
+        [Test]
+        public void Validate_Json_ToString()
+        {
+            //Arrange
+            CustomerModel customerModel = new CustomerModel();
+
+            //Act
+            customerModel.NominatedTitle = "Seattle Aquarium ";
+            string data = customerModel.ToString();
+
+            //Assert
+            Assert.AreEqual(true, data.Contains("Seattle"));
+        }
+    }
+}
