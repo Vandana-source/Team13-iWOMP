37af67783fc2dce526e9aecf0297a95dcf17ff05
myasha <myasha@gmail.com>
2023-11-18 10:14:24 -0800

    Update site.js

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+const map = new maplibregl.Map({
+    container: 'map', // container id
+    style: 'https://demotiles.maplibre.org/style.json', // style URL
+    center: [0, 0], // [longitude, latitude]
+    zoom: 1 // starting zoom level
+});
