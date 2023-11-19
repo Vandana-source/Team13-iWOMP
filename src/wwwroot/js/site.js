c41207bd78ea4cf291042936715cc7e1c14d36d6
myasha <myasha@gmail.com>
2023-11-19 09:50:55 -0800

    Updated map libre data source in site.js

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -2,12 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+///<summary>
+/// map libre
+///</summary>
 const map = new maplibregl.Map({
-    container: 'map', // container id
-    style: 'https://demotiles.maplibre.org/style.json', // style URL
-    center: [0, 0], // [longitude, latitude]
-    zoom: 1 // starting zoom level
+
+    // container id
+    container: 'map', 
+
+    // MapTiler style URL with key
+    style: 'https://api.maptiler.com/maps/streets-v2/style.json?key=K4IpGwkgz6Fw1K2vSoDR', 
+
+    // [longitude, latitude]
+    center: [-122.3212675,47.6033376], 
+    
+    // starting zoom level
+    zoom: 10 
 });
 
 
+
+// Adds the nav controls for the map 
 map.addControl(new maplibregl.NavigationControl());
