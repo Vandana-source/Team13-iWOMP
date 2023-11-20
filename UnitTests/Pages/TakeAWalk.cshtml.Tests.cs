bb83a7c2a36a83b884f35731cfb28a2b67cdeba0
myasha <myasha@gmail.com>
2023-11-20 12:32:12 -0800

    Updated TakeAWalk.cshtml.Tests.cs

--- UnitTests/Pages/TakeAWalk.cshtml.Tests.cs
+++ UnitTests/Pages/TakeAWalk.cshtml.Tests.cs
@@ -1,6 +1,106 @@
-namespace UnitTests.Pages;
+using System.Diagnostics;
 
-public class TakeAWalk_cshtml_Tests
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+using NUnit.Framework;
+
+using Moq;
+
+using TakeABreak.WebSite.Pages;
+using TakeABreak.WebSite.Services;
+
+namespace UnitTests.Pages.TakeAWalk
 {
-    
+    // <summary>
+    /// Unit testing for TakeAWalk page
+    /// </summary>
+    public class TakeAWalkTests
+    {
+        #region TestSetup
+        public static IUrlHelperFactory urlHelperFactory;
+        public static DefaultHttpContext httpContextDefault;
+        public static IWebHostEnvironment webHostEnvironment;
+        public static ModelStateDictionary modelState;
+        public static ActionContext actionContext;
+        public static EmptyModelMetadataProvider modelMetadataProvider;
+        public static ViewDataDictionary viewData;
+        public static TempDataDictionary tempData;
+        public static PageContext pageContext;
+
+        public static TakeAWalkModel pageModel;
+
+        /// <summary>
+        /// Set up test intialize
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            httpContextDefault = new DefaultHttpContext()
+            {
+                TraceIdentifier = "trace",
+                //RequestServices = serviceProviderMock.Object,
+            };
+            httpContextDefault.HttpContext.TraceIdentifier = "trace";
+
+            modelState = new ModelStateDictionary();
+
+            actionContext = new ActionContext(httpContextDefault, httpContextDefault.GetRouteData(), new PageActionDescriptor(), modelState);
+
+            modelMetadataProvider = new EmptyModelMetadataProvider();
+            viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            tempData = new TempDataDictionary(httpContextDefault, Mock.Of<ITempDataProvider>());
+
+            pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData,
+                HttpContext = httpContextDefault
+            };
+
+            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
+            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
+            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net7.0/wwwroot");
+            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
+
+            var MockLoggerDirect = Mock.Of<ILogger<TakeAWalkModel>>();
+            JsonFileProductService productService;
+
+            productService = new JsonFileProductService(mockWebHostEnvironment.Object);
+
+            pageModel = new TakeAWalkModel(MockLoggerDirect)
+            {
+                PageContext = pageContext,
+                TempData = tempData,
+            };
+        }
+
+        #endregion TestSetup
+
+        /// <summary>
+        /// Tests OnGet with a valid product, should return valid product data
+        /// </summary>
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+    }
 }
