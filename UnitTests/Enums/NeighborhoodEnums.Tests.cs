53955a9d3e7e18c7a7ee81ec5b9499649cd3d086
myasha <myasha@gmail.com>
2023-11-21 23:23:32 -0800

    Created NeighborhoodEnums.Tests.cs

---
+++ UnitTests/Enums/NeighborhoodEnums.Tests.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework;
+using TakeABreak.WebSite.Enums;
+
+namespace UnitTests.Enums
+{
+    /// <summary>
+    /// Class to provide unit testing of NoiseLevelEnum.cs
+    /// </summary>
+    public class NeighborhoodEnums
+    {
+        #region TestSetup
+        /// <summary>
+        /// Test Setup
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+
+        #region StaticMethodsTests
+        [Test]
+        /// <summary>
+        /// Test that checks functionality of GetName
+        /// </summary>
+        public void Valid_Enum_Should_Return_Correct_Name()
+        {
+            // Arrange
+          
+
+            // Act
+
+            // Assert
+     
+        }
+        #endregion StaticMethodTests
+    }
+}
