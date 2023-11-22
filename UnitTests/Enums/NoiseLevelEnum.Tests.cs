8632bd244496379f945b535809cb1c72e1d51134
Vandana-source <145959980+Vandana-source@users.noreply.github.com>
2023-11-21 20:51:56 -0800

    Created new test class for NoiseLevelEnum.cs and written test case in NoiseLevelEnum.Tests.cs

---
+++ UnitTests/Enums/NoiseLevelEnum.Tests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using TakeABreak.WebSite.Enums;
+
+namespace UnitTests.Enums
+{
+    /// <summary>
+    /// Class to provide unit testing of NoiseLevelEnum.cs
+    /// </summary>
+    public class NoiseLevelEnumTests
+    {
+        #region TestSetup
+        /// <summary>
+        /// Test Setup
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+
+        #region StaticMethodsTests
+        [Test]
+        /// <summary>
+        /// Test that checks functionality of GetName
+        /// </summary>
+        public void Valid_Enum_Should_Return_Correct_Name()
+        {
+            // Arrange
+            NoiseLevel testEnum = NoiseLevel.VeryLoud;
+
+            // Act
+
+            // Assert
+            Assert.AreEqual("VeryLoud", testEnum.ToString());
+        }
+        #endregion StaticMethodTests
+    }
+}
