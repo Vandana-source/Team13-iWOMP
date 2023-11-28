4c682fc0491defe2c9c85c327c836cf7f96ea436
Vandana-source <145959980+Vandana-source@users.noreply.github.com>
2023-11-28 01:45:07 -0800

    Created new test class for MapController and written UTs for Get in MapControllerTests.cs

---
+++ UnitTests/Controllers/MapControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using TakeABreak.WebSite.Controllers;
+using TakeABreak.WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static TakeABreak.WebSite.Controllers.MapController;
+
+namespace UnitTests.Controllers
+{
+
+    /// <summary>
+    /// Class containing unit test cases for MapController
+    /// </summary>
+    public class MapControllerTests
+    {
+
+        //Creating an instance
+        public static MapController testMapController;
+
+        /// <summary>
+        /// Test initialize
+        /// </summary>
+        #region TestSetup
+
+        [SetUp]
+        public void Testinitialize()
+        {
+            testMapController = new MapController(TestHelper.MapModelService);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Testing if get is valid should return geoJson data
+        /// </summary>
+        [Test]
+        public void Get_Valid_Should_Return_List_Of_Products()
+        {
+            //Arrange
+            var result = testMapController.GetGeoJSON();
+
+            //Act
+
+            //Assert
+            Assert.NotNull(result);
+        }
+    }
+}
