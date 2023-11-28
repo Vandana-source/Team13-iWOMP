cfcc046428333d777bf8c3417f5d6c3e648b7fd5
myasha <myasha@gmail.com>
2023-11-28 15:07:56 -0800

    updated undefined enum in NoiseLevelEnum.cs

--- src/Enums/NoiseLevelEnum.cs
+++ src/Enums/NoiseLevelEnum.cs
@@ -4,26 +4,26 @@ using System.ComponentModel.DataAnnotations;
 namespace TakeABreak.WebSite.Enums
 {
     /// <summary>
-    /// Enum for strings describing noise levels
-    /// </summary>
-    public enum NoiseLevelEnum
-    {
-        [Display(Name = "Undefined")] 
-        Undefined = 0,
+        /// Enum for strings describing noise levels
+        /// </summary>
+        public enum NoiseLevelEnum
+        {
+            [Display(Name = "Undefined")] 
+            Undefined = 0, // Added Undefined as the first entry
 
-        [Display(Name = "Quiet")]
-        Quiet = 1,
+            [Display(Name = "Quiet")]
+            Quiet = 1,
 
-        [Display(Name = "Moderate")]
-        Moderate = 3,
+            [Display(Name = "Moderate")]
+            Moderate = 3,
 
-        [Display(Name = "Loud")]
-        Loud = 5,
+            [Display(Name = "Loud")]
+            Loud = 5,
 
-        [Display(Name = "Very Loud")]
-        VeryLoud = 7,
+            [Display(Name = "Very Loud")]
+            VeryLoud = 7,
 
-        [Display(Name = "Extremely Loud")]
-        ExtremelyLoud = 9,
+            [Display(Name = "Extremely Loud")]
+            ExtremelyLoud = 9,
+        }
     }
-}
