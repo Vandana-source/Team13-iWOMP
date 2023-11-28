e702a19ebd96dea2c88d6d3f337fd1f75b4ccd1c
myasha <myasha@gmail.com>
2023-11-28 12:04:00 -0800

    updated route logic in MapStuff.js

--- src/wwwroot/js/mapStuff.js
+++ src/wwwroot/js/mapStuff.js
@@ -1,3 +1,8 @@
+// Function to create and add custom markers
+// Global variables to store start and end points
+let startPoint = null;
+let endPoint = null;
+
 // Create a new map instance
 const map = new maplibregl.Map({
     container: 'map',
@@ -17,16 +22,27 @@ function addCustomMarkers(geojsonData) {
         markerElement.className = type.toLowerCase() + '-icon'; // e.g., 'bench-icon'
 
         // Create the marker and set its properties
-        new maplibregl.Marker(markerElement, {
-            anchor: 'bottom'
-        })
+        new maplibregl.Marker(markerElement, { anchor: 'bottom' })
             .setLngLat(coordinates)
             .addTo(map);
 
-        // Optional: Add popup or event listeners to the marker here
+        // Add click event listener to each marker
+        markerElement.addEventListener('click', () => {
+            if (!startPoint) {
+                // Set the start point
+                startPoint = coordinates;
+            } else {
+                // Set the end point and draw the route
+                endPoint = coordinates;
+                drawRoute(startPoint, endPoint);
+
+                // Reset the start and end points for the next route
+                startPoint = null;
+                endPoint = null;
+            }
+        });
     });
 }
-
 // Function to update the location type filter
 function updateLocationType(locationType) {
     // Select all markers
@@ -58,4 +74,52 @@ map.on('load', function () {
 
     // Add navigation controls to the map
     map.addControl(new maplibregl.NavigationControl());
+});
+
+// Function to draw a route on the map
+// Function to draw a route on the map using Geoapify API
+// Function to draw a route on the map using Geoapify API
+function drawRoute(start, end) {
+    const requestOptions = {
+        method: 'GET',
+    };
+
+    const startCoords = `${start[1]},${start[0]}`; // Latitude, Longitude
+    const endCoords = `${end[1]},${end[0]}`; // Latitude, Longitude
+
+    fetch(`https://api.geoapify.com/v1/routing?waypoints=${startCoords}%7C${endCoords}&mode=drive&apiKey=1ca889777476478d8b34a21892745067`, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            const route = result.features[0]; // Assuming the route is the first feature
+            if (map.getSource('route')) {
+                map.getSource('route').setData(route);
+            } else {
+                map.addLayer({
+                    id: 'route',
+                    type: 'line',
+                    source: {
+                        type: 'geojson',
+                        data: route
+                    },
+                    layout: {
+                        'line-join': 'round',
+                        'line-cap': 'round'
+                    },
+                    paint: {
+                        'line-color': '#1DB7DD',
+                        'line-width': 5
+                    }
+                });
+            }
+        })
+        .catch(error => console.error('Error fetching route:', error));
+}
+
+// Function to clear the route from the map
+map.getContainer().addEventListener('click', (e) => {
+    if (e.target.className.includes('-icon')) return; // Ignore clicks on icons
+    if (map.getSource('route')) {
+        map.removeLayer('route');
+        map.removeSource('route');
+    }
 });
