f3232de142058c632a33eaefe34cfc7816b42a43
myasha <myasha@gmail.com>
2023-11-28 09:19:04 -0800

    added ability to filter by icon in mapstuff.js

--- src/wwwroot/js/mapStuff.js
+++ src/wwwroot/js/mapStuff.js
@@ -6,61 +6,56 @@ const map = new maplibregl.Map({
     zoom: 11
 });
 
+// Function to create and add custom markers
+function addCustomMarkers(geojsonData) {
+    geojsonData.features.forEach(feature => {
+        const type = feature.properties.LocationType;
+        const coordinates = feature.geometry.coordinates;
+
+        // Create a div element for the marker
+        const markerElement = document.createElement('div');
+        markerElement.className = type.toLowerCase() + '-icon'; // e.g., 'bench-icon'
+
+        // Create the marker and set its properties
+        new maplibregl.Marker(markerElement, {
+            anchor: 'bottom'
+        })
+            .setLngLat(coordinates)
+            .addTo(map);
+
+        // Optional: Add popup or event listeners to the marker here
+    });
+}
+
+// Function to update the location type filter
+function updateLocationType(locationType) {
+    // Select all markers
+    const allMarkers = document.querySelectorAll('.bench-icon, .restroom-icon, .table-icon, .other-icon');
+
+    allMarkers.forEach(marker => {
+        if (locationType === null) {
+            // Show all markers
+            marker.style.display = 'block';
+        } else {
+            // Show markers based on the selected type
+            if (marker.classList.contains(locationType.toLowerCase() + '-icon')) {
+                marker.style.display = 'block';
+            } else {
+                marker.style.display = 'none';
+            }
+        }
+    });
+
+    // Update the button text
+    const selectedLocationTypeText = locationType === null ? 'All' : locationType;
+    document.getElementById('selectedLocationType').innerText = selectedLocationTypeText;
+}
+
 // Event handler when the map style loads
 map.on('load', function () {
-    console.log('Map loaded');
+    // Assuming geojsonData is already defined and available
+    addCustomMarkers(geojsonData);
 
-    // Load a custom icon image
-    map.loadImage('https://api.geoapify.com/v1/icon/?type=awesome&color=%233dd448&size=large&icon=restroom&scaleFactor=2&apiKey=1ca889777476478d8b34a21892745067', function (error, image) {
-        if (error) {
-            console.error('An error occurred while loading the image:', error);
-            return; // Exit the function if the image cannot be loaded
-        }
-
-        // Add the custom icon image to the map
-        map.addImage('Custom_icon', image);
-
-        // Add the GeoJSON source
-        map.addSource('myGeoJSON', {
-            type: 'geojson',
-            data: geojsonData
-        });
-
-        // Add a layer to display the GeoJSON features specifically for restrooms
-        map.addLayer({
-            id: 'Restroom',
-            type: 'symbol',
-            source: 'myGeoJSON',
-            layout: {
-                'icon-image': 'Custom_icon',
-                'text-field': ['get', 'Title'], // Display the 'Title' property as text
-                'text-size': 12,
-                'text-offset': [0, 1.5],
-                'icon-size': 0.05
-            },
-            filter: ['==', ['downcase', ['get', 'LocationType']], 'restroom']
-        });
-
-        // Loop through your GeoJSON data to add markers
-        geojsonData.features.forEach(feature => {
-            if (feature.properties && feature.properties.LocationType && feature.geometry && feature.geometry.type === "Point") {
-                if (feature.properties.LocationType.toLowerCase() === 'restroom') {
-                    const coords = feature.geometry.coordinates;
-
-                    const restroomIcon = document.createElement('div');
-                    restroomIcon.className = 'restroom';
-
-                    new maplibregl.Marker(restroomIcon, {
-                        anchor: 'bottom',
-                        offset: [0, -34.5] // Adjust as needed for the marker's anchor point
-                    })
-                        .setLngLat(coords)
-                        .addTo(map);
-                }
-            }
-        });
-    });
+    // Add navigation controls to the map
+    map.addControl(new maplibregl.NavigationControl());
 });
-
-// Add navigation controls to the map
-map.addControl(new maplibregl.NavigationControl());
